bef3018a64de3d812018b3a4b89e4f9854832338
manash33pratim <manash33pratim@gmail.com>
2023-02-25 03:53:57 +0530

    intropage

--- lib/onboarding_screen.dart
+++ lib/onboarding_screen.dart
@@ -1,5 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:taskmanagementapp/introduction_screen/page1.dart';
+import 'package:taskmanagementapp/introduction_screen/page2.dart';
+import 'package:taskmanagementapp/introduction_screen/page3.dart';
+import 'package:taskmanagementapp/page/auth.dart';
 
 class Onboarding extends StatefulWidget {
   const Onboarding({super.key});
@@ -8,9 +15,54 @@ class Onboarding extends StatefulWidget {
   State<Onboarding> createState() => _OnboardingState();
 }
 
+PageController _controller = PageController();
+
 class _OnboardingState extends State<Onboarding> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: Stack(
+      children: [
+        PageView(
+          controller: _controller,
+          children: [
+            IntroPage1(),
+            IntroPage2(),
+            IntroPage3(),
+          ],
+        ),
+        Container(
+          alignment: Alignment(0, 0.8),
+          child: SmoothPageIndicator(
+            controller: _controller,
+            count: 3,
+          ),
+        ),
+        Container(
+          alignment: Alignment(0.7, 0.8),
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => authontication()));
+            },
+            child: Text('skip'),
+          ),
+        )
+      ],
+    ));
   }
+}
+
+class authontication extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            return AuthPage();
+          },
+        ),
+      );
 }
